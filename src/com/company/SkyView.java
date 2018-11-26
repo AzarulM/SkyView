d74f3233b267af65354a3384f37182476997821c
BT_1N3_30@bktech.local <BT_1N3_30@bktech.local>
2018-11-26 11:44:59 -0500

    Initial commit

---
+++ src/com/company/SkyView.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class SkyView {
+
+    private double [][] view;
+
+    public SkyView(int numRows, int numCols, double [] scanned)
+    {
+        view = new double [numRows][numCols];
+        int  = 0;
+    }
+}
